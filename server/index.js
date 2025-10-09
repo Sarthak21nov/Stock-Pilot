7355517157d5184a0c2fec0f19f973432cf1f77d
Sarthak Yash Srivastava <115006366+Sarthak21nov@users.noreply.github.com>
2025-10-09 10:16:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ mongoose.connection.on("disconnected", ()=>{
 // Middleware
 const corsOptions = {
   // origin: 'http://localhost:5173', 
-  origin: 'https://stock-pilot-rho.vercel.app/',  
+  origin: 'https://stock-pilot-rho.vercel.app',  
   credentials: true
 };
 
