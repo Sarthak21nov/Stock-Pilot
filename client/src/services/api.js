7706bafc891fcdfa2637189ac5fcf8b480b79feb
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2025-10-24 11:49:35 +0530

    Changed Backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://stock-pilot-2.onrender.com';
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://stock-pilot-nine.vercel.app/';
 
 // const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
