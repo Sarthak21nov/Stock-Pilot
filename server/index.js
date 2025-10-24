254f93209bc46cd131da63bb6ccdfa7836e80f2b
Sarthak Yash Srivastava <sarthak.nov21@gmail.com>
2025-10-24 12:01:53 +0530

    Fixed Cors settings -v2

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ mongoose.connection.on("disconnected", ()=>{
 // Middleware
 const corsOptions = {
   // origin: 'http://localhost:5173', 
-  origin: ['https://stock-pilot-nine.vercel.app','https://main.d3sgipjjeevgzg.amplifyapp.com', 'http://localhost:5173'],  
+  origin: ['https://stock-pilot-4oj8.vercel.app','https://main.d3sgipjjeevgzg.amplifyapp.com', 'http://localhost:5173'],  
   credentials: true
 };
 
